034e151644cff8861fc4328776ea34eac7ba5004
wiwi.juwita <wiwijuwita105@gmail.com>
2024-07-05 11:13:10 +0700

    feat: grpc crud

---
+++ session9-crud-user-grpc/middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net/http"
+	"session9-crud-user-grpc/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Verifikasi token (misalnya, cocokkan dengan token yang diharapkan)
+		username, password, ok := c.Request.BasicAuth()
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization basic token required"})
+			c.Abort()
+			return
+		}
+
+		isValid := (username == config.AuthBasicUsername) && (password == config.AuthBasicPassword)
+		if !isValid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization token"})
+			c.Abort()
+			return
+		}
+
+		// Lanjutkan ke handler berikutnya jika token valid
+		c.Next()
+	}
+}
---
+++ session9-crud-user-grpc/router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	handler "session9-crud-user-grpc/handler/gin"
+	"session9-crud-user-grpc/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(r *gin.Engine, userHandler handler.IUserHandler) {
+	// Mengatur endpoint publik untuk pengguna
+	usersPublicEndpoint := r.Group("/users")
+	// Rute untuk mendapatkan pengguna berdasarkan ID
+	usersPublicEndpoint.GET("/:id", userHandler.GetUser)
+	// Rute untuk mendapatkan semua pengguna
+	usersPublicEndpoint.GET("", userHandler.GetAllUsers)
+	usersPublicEndpoint.GET("/", userHandler.GetAllUsers)
+
+	// Mengatur endpoint privat untuk pengguna dengan middleware autentikasi
+	usersPrivateEndpoint := r.Group("/users")
+	// Menambahkan middleware autentikasi untuk endpoint privat
+	usersPrivateEndpoint.Use(middleware.AuthMiddleware())
+	// Rute untuk membuat pengguna baru
+	usersPrivateEndpoint.POST("", userHandler.CreateUser)
+	usersPrivateEndpoint.POST("/", userHandler.CreateUser)
+	// Rute untuk memperbarui pengguna berdasarkan ID
+	usersPrivateEndpoint.PUT("/:id", userHandler.UpdateUser)
+	// Rute untuk menghapus pengguna berdasarkan ID
+	usersPrivateEndpoint.DELETE("/:id", userHandler.DeleteUser)
+}
